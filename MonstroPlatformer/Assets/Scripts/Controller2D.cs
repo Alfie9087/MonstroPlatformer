d941608dd9c458708ec7b356b072ded3b389c264
Alfie9087 <antonio.castillo@obf.ateneo.edu>
2022-06-16 02:06:28 +0800

    Added horizontal collisions and left and right input
    -improved public void move
    -added movement using horizontal input (GetAxisRaw)
    -added horizontalcollisions function that basically uses the same logic as vertical collisions

--- MonstroPlatformer/Assets/Scripts/Controller2D.cs
+++ MonstroPlatformer/Assets/Scripts/Controller2D.cs
@@ -33,10 +33,36 @@ public class Controller2D : MonoBehaviour
     public void Move(Vector3 velocity)
     {     
         UpdateRaycastOrigins();
-
-        VerticalCollisions(ref velocity);
+        if(velocity.x != 0){
+            HorizontalCollisions(ref velocity);
+        }
+        if(velocity.y != 0){
+            VerticalCollisions(ref velocity);
+        }
         transform.Translate (velocity);
     }
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        //get direction and length using the velocity y
+        float directionX =  Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i ++){
+            Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horitalRaySpacing * i);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin,Vector2.right*directionX,rayLength,collisionMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+            if(hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+
 
     //ref = uses actual variable instead of copying it. Basically like pointers
     void VerticalCollisions(ref Vector3 velocity)
@@ -50,7 +76,7 @@ public class Controller2D : MonoBehaviour
             rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
 
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin,Vector2.up*directionY,rayLength,collisionMask);
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right *verticalRaySpacing * i, Vector2.up * -2, Color.red);
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
 
             if(hit)
             {
--- MonstroPlatformer/Assets/Scripts/Player.cs
+++ MonstroPlatformer/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     float gravity= -10;
+    float moveSpeed= 6;
     Vector3 velocity;
 
 
@@ -21,6 +22,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        
+        velocity.x = input.x * moveSpeed;
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
